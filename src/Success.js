0202943c29067d5e792f59387265939de571ebe7
Alan <atang8013@gmail.com>
2021-03-10 10:41:48 -0500

    red circle over spotify screenshot

--- src/Success.js
+++ src/Success.js
@@ -1,9 +1,22 @@
 import React from "react";
+import screenshot from "./assets/new-playlist-screenshot.png";
+import redCircle from "./assets/red-circle.gif";
 
 export default function Success() {
   return (
     <div>
-      <h1>Playlist Added</h1>
+      <h1 class="p-20 text-white text-7xl font-bold flex justify-center text-center ... ">
+        Go ahead and check your spotify playlists!
+      </h1>
+      <div class="">
+        <div class="relative flex justify-center">
+          <img
+            class="absolute top-9 left-0  rounded-lg shadow-2xl  object-none object-center"
+            src={screenshot}
+          />
+          <img class="max-w-1/2  absolute top-0 left-0" src={redCircle} />
+        </div>
+      </div>
     </div>
   );
 }
